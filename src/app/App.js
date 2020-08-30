510da6dd3fdaa5b7f7409412585cbfc318da91b7
Jizong Liang <Liang_Jizong@comcast.com>
2020-08-30 15:49:06 -0400

    updated react history and subdir

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from '../context/auth';
-import Login from './home/pages/Login';
-import Register from './home/pages/Register';
-import Home from './home/pages/Home';
-import AuthRoute from '../util/AuthRoute';
-import PublicRoute from '../util/PublicRoute';
-import * as routes from '../constants/routes';
-import PageContainer from './shared/components/PageContainer';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "../context/auth";
+import Login from "./home/pages/Login";
+import Register from "./home/pages/Register";
+import Home from "./home/pages/Home";
+import AuthRoute from "../util/AuthRoute";
+import PublicRoute from "../util/PublicRoute";
+import * as routes from "../constants/routes";
+import PageContainer from "./shared/components/PageContainer";
+import { createBrowserHistory } from "history";
 
 const App = () => (
   <AuthProvider>
@@ -20,5 +21,9 @@ const App = () => (
     </PageContainer>
   </AuthProvider>
 );
+
+export const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL,
+});
 
 export default App;
